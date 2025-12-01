ba0ac75f388163464671b1ce5fbf844ba49d3bdc
Gustavo <gustavossantos41@outlook.com>
2025-12-01 11:54:18 -0300

    Versão da Grafeno implentada, atual versão de produção

--- EventoContratosDataCorteVencVORTX_WK/EventoContratosDataCorteVencVORTX/src/br/com/metropolitana/contratosdatavenc/MainEventoContratosDataVenc.java
+++ EventoContratosDataCorteVencVORTX_WK/EventoContratosDataCorteVencVORTX/src/br/com/metropolitana/contratosdatavenc/MainEventoContratosDataVenc.java
@@ -2,18 +2,32 @@ package br.com.metropolitana.contratosdatavenc;
 
 import java.math.BigDecimal;
 import java.sql.Timestamp;
+import java.time.Instant;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.time.temporal.ChronoUnit;
+import java.util.Date;
 
 import br.com.sankhya.extensions.eventoprogramavel.EventoProgramavelJava;
+import br.com.sankhya.jape.EntityFacade;
 import br.com.sankhya.jape.event.PersistenceEvent;
 import br.com.sankhya.jape.event.TransactionContext;
+import br.com.sankhya.jape.sql.NativeSql;
 import br.com.sankhya.jape.vo.DynamicVO;
 import br.com.sankhya.jape.wrapper.JapeFactory;
 import br.com.sankhya.jape.wrapper.JapeWrapper;
 import br.com.sankhya.modelcore.util.DynamicEntityNames;
+import br.com.sankhya.modelcore.util.EntityFacadeFactory;
 
 public class MainEventoContratosDataVenc implements EventoProgramavelJava{
+	
+    JapeWrapper parceiroDAO = JapeFactory.dao(DynamicEntityNames.PARCEIRO);
+    JapeWrapper financeiroDAO = JapeFactory.dao(DynamicEntityNames.FINANCEIRO);
+    JapeWrapper cabDAO = JapeFactory.dao(DynamicEntityNames.CABECALHO_NOTA);
+    JapeWrapper movSinteticosDAO = JapeFactory.dao("AD_FINSINTETICOSGRAFENO");
+	JapeWrapper configDAO = JapeFactory.dao("AD_CONFIGSINTGRAF");
+
 
 	@Override
 	public void afterDelete(PersistenceEvent arg0) throws Exception {
@@ -23,50 +37,128 @@ public class MainEventoContratosDataVenc implements EventoProgramavelJava{
 
 	@Override
 	public void afterInsert(PersistenceEvent event) throws Exception {
-		
-		JapeWrapper parceiroDAO = JapeFactory.dao(DynamicEntityNames.PARCEIRO);
-	    JapeWrapper financeiroDAO = JapeFactory.dao(DynamicEntityNames.FINANCEIRO);
 	    
 	    DynamicVO financeiroVO = (DynamicVO) event.getVo();
-	    DynamicVO parceiroVO = parceiroDAO.findByPK(financeiroVO.asBigDecimal("CODPARC"));
-        
-	    if ((financeiroVO.asBigDecimal("NUMCONTRATO") != null && financeiroVO.asBigDecimal("NUMCONTRATO").compareTo(BigDecimal.ZERO) != 0) || existefaturHH(financeiroVO.asBigDecimal("NUNOTA"))) {
-	        LocalDate dataVencimento = null;
-	        LocalDate hoje = LocalDate.now();
-	     	        
-	        if (parceiroVO.asBigDecimal("AD_DTCORTE_VENC") != null) {
-	            int dataCorte = parceiroVO.asBigDecimal("AD_DTCORTE_VENC").intValue();
-	            LocalDate dataCorteAtual = LocalDate.of(hoje.getYear(), hoje.getMonth(), dataCorte);
-	            if (ChronoUnit.DAYS.between(hoje, dataCorteAtual) < 15) {
-	                dataVencimento = hoje.plusDays(15);
-	            } else {
-	                dataVencimento = dataCorteAtual;
-	            }
-	        } 
-	        else if (parceiroVO.asBigDecimal("AD_DIASCORRIDOSVENC") != null) {
-	            int diasCorridos = parceiroVO.asBigDecimal("AD_DIASCORRIDOSVENC").intValue();
-	            dataVencimento = hoje.plusDays(diasCorridos);
+	    
+	    if (deveAtualizarDataVencimento(financeiroVO)) {
+	        Timestamp dataVencimento = calcularDataVencimento(financeiroVO);
+	        
+	        if (dataVencimento != null) {
+	            atualizarDataVencimento(financeiroVO, dataVencimento);
 	        }
-	        else {
-	            dataVencimento = hoje.plusDays(15);
-	        }
-	        
-	        dataVencimento = ajustarParaUltimoDiaDoMes(dataVencimento);
-	        
-	        Timestamp timestampVencimento = Timestamp.valueOf(dataVencimento.atStartOfDay());
-	        	        
-	        try {
-				financeiroDAO.prepareToUpdate(financeiroVO)
-				.set("DTVENC", timestampVencimento)
-				.update();
-			} catch (Exception e) {
-				System.out.println("Erro data de corte - Metropolitana/Gustavo: "+e.getMessage());
-				e.printStackTrace();
-			}
-	        
 	    }
-		
+	    
+	    if(deveAtualizarGrafeno(financeiroVO)) {
+	    	atualizarInformacoesGrafeno(financeiroVO);
+	    }
 	}
+	
+	private void atualizarInformacoesGrafeno(DynamicVO financeiroVO) throws Exception {
+	    
+	    BigDecimal nunota = financeiroVO.asBigDecimal("NUNOTA");
+	    DynamicVO cabVO = cabDAO.findByPK(financeiroVO.asBigDecimal("NUNOTA"));
+	    
+	    boolean isOriginador = determinarSeOriginador(nunota);
+	    
+	    EntityFacade dwfFacade = EntityFacadeFactory.getDWFFacade();
+	    NativeSql nativeSql = new NativeSql(dwfFacade.getJdbcWrapper());
+	    
+	    Timestamp dtVencOriginador = calcularDataComDiasCorridos(new Date(), 15);
+	    Timestamp dtNeg = new Timestamp(System.currentTimeMillis());
+	    
+	    nativeSql.setNamedParameter("NUFIN", financeiroVO.asBigDecimal("NUFIN"));
+	    nativeSql.setNamedParameter("CODTIPTIT", isOriginador ? new BigDecimal(4) : new BigDecimal(22));
+	    nativeSql.setNamedParameter("CODBCO", isOriginador ? new BigDecimal(310) : new BigDecimal(274));
+	    nativeSql.setNamedParameter("CODCTABCOINT", isOriginador ? new BigDecimal(99) : new BigDecimal(100));
+	    nativeSql.setNamedParameter("DTVENC", isOriginador ? dtVencOriginador : dtNeg);
+	    nativeSql.setNamedParameter("AD_IDSINTETICO", cabVO.asBigDecimal("AD_IDSINTETICO"));
+	    
+	    nativeSql.executeUpdate(
+	        "UPDATE TGFFIN SET CODTIPTIT = :CODTIPTIT, " +
+	        "CODBCO = :CODBCO, " +
+	        "CODCTABCOINT = :CODCTABCOINT, " +
+	        "DTVENC = :DTVENC, " +
+	        "AD_IDSINTETICO = :AD_IDSINTETICO " +
+	        "WHERE NUFIN = :NUFIN"
+	    );
+	}
+
+
+	private boolean determinarSeOriginador(BigDecimal nunota) throws Exception {
+	    DynamicVO sinteticoVO = movSinteticosDAO.findOne("NUNOTA = ?", nunota);
+	    
+	    return sinteticoVO != null && "S".equals(sinteticoVO.asString("ORIGINADOR"));
+	}
+
+	private Timestamp calcularDataComDiasCorridos(Date dataBase, int dias) {
+	    Instant instant = dataBase.toInstant();
+	    LocalDateTime localDateTime = instant.atZone(ZoneId.systemDefault())
+	                                        .toLocalDateTime()
+	                                        .plusDays(dias);
+	    return Timestamp.valueOf(localDateTime);
+	}
+
+	private boolean deveAtualizarDataVencimento(DynamicVO financeiroVO) throws Exception {
+	    return (financeiroVO.asBigDecimal("NUMCONTRATO") != null && 
+	            financeiroVO.asBigDecimal("NUMCONTRATO").compareTo(BigDecimal.ZERO) != 0) || 
+	            existefaturHH(financeiroVO.asBigDecimal("NUNOTA"));
+	}
+	
+	private boolean deveAtualizarGrafeno(DynamicVO financeiroVO) throws Exception {
+		
+		if(financeiroVO.asBigDecimal("NUNOTA") == null) {
+			return false;
+		}
+	    DynamicVO cabVO = cabDAO.findByPK(financeiroVO.asBigDecimal("NUNOTA"));
+	    
+	    return cabVO != null && cabVO.asBigDecimal("AD_IDSINTETICO") != null;
+	
+	}
+
+	private Timestamp calcularDataVencimento(DynamicVO financeiroVO) throws Exception {
+	    DynamicVO parceiroVO = parceiroDAO.findByPK(financeiroVO.asBigDecimal("CODPARC"));
+	    
+	    if (parceiroVO == null) {
+	        return null;
+	    }
+	    
+	    LocalDate dataVencimento = null;
+	    LocalDate hoje = LocalDate.now();
+	    
+	    if (parceiroVO.asBigDecimal("AD_DTCORTE_VENC") != null) {
+	        int dataCorte = parceiroVO.asBigDecimal("AD_DTCORTE_VENC").intValue();
+	        LocalDate dataCorteAtual = LocalDate.of(hoje.getYear(), hoje.getMonth(), dataCorte);
+	        
+	        if (ChronoUnit.DAYS.between(hoje, dataCorteAtual) < 15) {
+	            dataVencimento = hoje.plusDays(15);
+	        } else {
+	            dataVencimento = dataCorteAtual;
+	        }
+	    } 
+	    else if (parceiroVO.asBigDecimal("AD_DIASCORRIDOSVENC") != null) {
+	        int diasCorridos = parceiroVO.asBigDecimal("AD_DIASCORRIDOSVENC").intValue();
+	        dataVencimento = hoje.plusDays(diasCorridos);
+	    }
+	    else {
+	        dataVencimento = hoje.plusDays(15);
+	    }
+	    
+	    dataVencimento = ajustarParaUltimoDiaDoMes(dataVencimento);
+	    
+	    return Timestamp.valueOf(dataVencimento.atStartOfDay());
+	}
+
+	private void atualizarDataVencimento(DynamicVO financeiroVO, Timestamp dataVencimento) {
+	    try {
+	        financeiroDAO.prepareToUpdate(financeiroVO)
+	            .set("DTVENC", dataVencimento)
+	            .update();
+	    } catch (Exception e) {
+	        System.out.println("Erro data de corte - Metropolitana/Gustavo: " + e.getMessage());
+	        e.printStackTrace();
+	    }
+	}
+
 
 	@Override
 	public void afterUpdate(PersistenceEvent event) throws Exception {
@@ -92,7 +184,6 @@ public class MainEventoContratosDataVenc implements EventoProgramavelJava{
 	}
 	
 	private LocalDate ajustarParaUltimoDiaDoMes(LocalDate data) {
-	    // Se o dia original for maior que o último dia do mês, ajusta para o último dia
 	    int diaOriginal = data.getDayOfMonth();
 	    int ultimoDiaMes = data.lengthOfMonth();
 	    
